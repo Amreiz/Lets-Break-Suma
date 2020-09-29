cdd5ca95bcfc1d311a675cb0b7d29a3ec6793201
Amreiz <amreizsuma@gmail.com>
2020-09-29 12:22:44 +0100

    Removing gitignore

---
+++ Let's Break Suma!/Assets/Scripts/Block.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    // paramaters
+    [SerializeField] AudioClip breakSound;
+
+    // cached references
+    Levels level;
+    GameStatus gameStatus;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Levels>();
+        level.CountBreakableBlocks();
+    }
+
+    private void DestroyBlock()
+    {
+        FindObjectOfType<GameStatus>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.CountDestroyedBlocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+}
---
+++ Let's Break Suma!/Assets/Scripts/Levels.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levels : MonoBehaviour {
+
+    // Serialized parameters for debugging
+    [SerializeField] int breakableBlocks;
+
+    // cached reference
+
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();    
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void CountDestroyedBlocks()
+    {
+        breakableBlocks--;
+
+        if(breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
+    }
+
+}
